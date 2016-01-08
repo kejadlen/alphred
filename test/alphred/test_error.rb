512a2c70109b78d328d1eb940a3a6281afb2a398
Alpha Chen <alpha.chen@gmail.com>
2016-01-07 21:06:33 -0800

    Use Ruby 2.0.0

--- test/alphred/test_error.rb
+++ test/alphred/test_error.rb
@@ -13,29 +13,29 @@ class TestError < AlphredTest
 
   def test_error
     icon_path = File.expand_path("../../../assets", __FILE__)
-    output = <<~OUTPUT
-      <?xml version="1.0" encoding="UTF-8"?>
-      <items>
-        <item>
-          <title>TestError::FakeException: OMG!</title>
-          <icon>#{File.join(icon_path, "close.png")}</icon>
-        </item>
-        <item arg="one">
-          <title>...one</title>
-          <subtitle>one</subtitle>
-          <icon>#{File.join(icon_path, "more.png")}</icon>
-        </item>
-        <item arg="two">
-          <title>...two</title>
-          <subtitle>two</subtitle>
-          <icon>#{File.join(icon_path, "more.png")}</icon>
-        </item>
-        <item arg="three">
-          <title>...three</title>
-          <subtitle>three</subtitle>
-          <icon>#{File.join(icon_path, "more.png")}</icon>
-        </item>
-      </items>
+    output = <<-OUTPUT
+<?xml version="1.0" encoding="UTF-8"?>
+<items>
+  <item>
+    <title>TestError::FakeException: OMG!</title>
+    <icon>#{File.join(icon_path, "close.png")}</icon>
+  </item>
+  <item arg="one">
+    <title>...one</title>
+    <subtitle>one</subtitle>
+    <icon>#{File.join(icon_path, "more.png")}</icon>
+  </item>
+  <item arg="two">
+    <title>...two</title>
+    <subtitle>two</subtitle>
+    <icon>#{File.join(icon_path, "more.png")}</icon>
+  </item>
+  <item arg="three">
+    <title>...three</title>
+    <subtitle>three</subtitle>
+    <icon>#{File.join(icon_path, "more.png")}</icon>
+  </item>
+</items>
     OUTPUT
 
     assert_output(output) do

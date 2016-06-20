5fbafa213ac4dec621f1d20db7b101472a2ed9b6
Alpha Chen <alpha.chen@gmail.com>
2016-06-19 17:05:03 -0700

    Style

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 source 'https://rubygems.org'
 
-ruby "2.0.0"
+ruby '2.0.0'
 
 # Specify your gem's dependencies in alphred.gemspec
 gemspec
 
 group :development do
-  gem "guard"
-  gem "guard-minitest"
-  gem "pry"
+  gem 'guard'
+  gem 'guard-minitest'
+  gem 'pry'
 end
--- lib/alphred.rb
+++ lib/alphred.rb
@@ -1,11 +1,11 @@
-require "builder"
+require 'builder'
 
-require_relative "alphred/version"
+require_relative 'alphred/version'
 
-require_relative "alphred/config"
-require_relative "alphred/error"
-require_relative "alphred/icon"
-require_relative "alphred/item"
-require_relative "alphred/items"
-require_relative "alphred/mods"
-require_relative "alphred/text"
+require_relative 'alphred/config'
+require_relative 'alphred/error'
+require_relative 'alphred/icon'
+require_relative 'alphred/item'
+require_relative 'alphred/items'
+require_relative 'alphred/mods'
+require_relative 'alphred/text'
